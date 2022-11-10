82f7ba569e2b45ce48167c904051169915d1f059
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2022-11-10 16:35:54 +0100

    feat: parse event body and build datadog event

--- extevents/extevents.go
+++ extevents/extevents.go
@@ -4,8 +4,17 @@
 package extevents
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV1"
+	"github.com/rs/zerolog/log"
+	"github.com/steadybit/event-kit/go/event_kit_api"
+	extension_kit "github.com/steadybit/extension-kit"
 	"github.com/steadybit/extension-kit/exthttp"
 	"net/http"
+	"os"
 )
 
 func RegisterEventListenerHandlers() {
@@ -13,7 +22,56 @@ func RegisterEventListenerHandlers() {
 	exthttp.RegisterHttpHandler("/events/experiment-completed", onExperimentCompleted)
 }
 
-func onExperimentStarted(w http.ResponseWriter, r *http.Request, _ []byte) {
+func onExperimentStarted(w http.ResponseWriter, r *http.Request, body []byte) {
+	// extract the request body to EventRequestBody
+	var event event_kit_api.EventRequestBody
+	err := json.Unmarshal(body, &event)
+	if err != nil {
+		exthttp.WriteError(w, extension_kit.ToError("Failed to decode event request body", err))
+		return
+	}
+
+	log.Debug().Msgf("Req %s body: %s", r, event)
+
+	tags := []string{
+		"Environment:" + event.Environment.Name,
+		"EventName:" + event.EventName,
+		"EventTime:" + event.EventTime.String(),
+		"EventId:" + event.Id.String(),
+		"Team:" + event.Team.Name,
+		"Tenant:" + event.Tenant.Name,
+		//"EndedTime:" + event.ExperimentExecution.EndedTime,
+		"ExecutionId:" + fmt.Sprintf("%f", event.ExperimentExecution.ExecutionId),
+		"ExperimentKey:" + event.ExperimentExecution.ExperimentKey,
+		"Hypothesis:" + event.ExperimentExecution.Hypothesis,
+		"ExperimentName:" + event.ExperimentExecution.Name,
+		"StartedTime:" + event.ExperimentExecution.StartedTime.String(),
+		string("State:" + event.ExperimentExecution.State),
+	}
+
+	for key, value := range event.ExperimentExecution.Variables {
+		tags = append(tags, key+":"+value)
+	}
+
+	datadogEventBody := datadogV1.EventCreateRequest{
+		Title: "Experiment started",
+		Text:  "An experiment has been started by the Steadybit platform",
+		Tags:  tags,
+	}
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV1.NewEventsApi(apiClient)
+	resp, _, err := api.CreateEvent(ctx, datadogEventBody)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `EventsApi.CreateEvent`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `EventsApi.CreateEvent`:\n%s\n", responseContent)
+
 	exthttp.WriteBody(w, "ok")
 }
 
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/rs/zerolog v1.27.0
 	github.com/steadybit/action-kit/go/action_kit_api/v2 v2.2.0
 	github.com/steadybit/discovery-kit/go/discovery_kit_api v1.1.0
+	github.com/steadybit/event-kit/go/event_kit_api v0.0.0-20221110145859-7bd695744de8
 	github.com/steadybit/extension-kit v1.4.0
 	github.com/stretchr/testify v1.8.0
 )
@@ -23,7 +24,6 @@ require (
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/steadybit/event-kit/go/event_kit_api v0.0.0-20221108164628-ca18aa6092e6 // indirect
 	github.com/stretchr/objx v0.4.0 // indirect
 	golang.org/x/net v0.0.0-20190108225652-1e06a53dbb7e // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
