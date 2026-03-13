bb2e2e7fd4e19ebf39ec50fdd226255601878119
Johannes Edmeier <joshiste@users.noreply.github.com>
2026-03-13 23:46:21 +0100

    feat: support if-none-match for the extension list endpoint (#190)
    Use a startup timestamp as ETag for the "/" index handler so that
    clients can avoid re-fetching the unchanged extension list.

--- main.go
+++ main.go
@@ -4,6 +4,8 @@
 package main
 
 import (
+	"time"
+
 	_ "github.com/KimMachineGun/automemlimit" // By default, it sets `GOMEMLIMIT` to 90% of cgroup's memory limit.
 	"github.com/rs/zerolog"
 	"github.com/steadybit/action-kit/go/action_kit_api/v2"
@@ -22,6 +24,8 @@ import (
 	"github.com/steadybit/extension-kit/extsignals"
 )
 
+var startedAt = time.Now().Format(time.RFC3339)
+
 func main() {
 	extlogging.InitZeroLog()
 	extbuild.PrintBuildInformation()
@@ -32,11 +36,12 @@ func main() {
 	config.ParseConfiguration()
 	config.ValidateConfiguration()
 
-	exthttp.RegisterHttpHandler("/", exthttp.GetterAsHandler(getExtensionList))
 	discovery_kit_sdk.Register(extmonitor.NewMonitorDiscovery())
 	action_kit_sdk.RegisterAction(extmonitor.NewMonitorStatusCheckAction())
 	action_kit_sdk.RegisterAction(extmonitor.NewMonitorDowntimeAction())
 	extevents.RegisterEventListenerHandlers()
+
+	exthttp.RegisterHttpHandler("/", exthttp.IfNoneMatchHandler(func() string { return startedAt }, exthttp.GetterAsHandler(getExtensionList)))
 
 	action_kit_sdk.RegisterCoverageEndpoints()
 	extsignals.ActivateSignalHandlers()
