c36a67f2f5b6b008195bb0084aca3673eae62aa6
Johannes Edmeier <joshiste@users.noreply.github.com>
2026-07-11 00:19:32 +0200

    refactor: register extension index via exthttp.RegisterRevisionedHandler (#225)
    Replace the hand-rolled startedAt + IfNoneMatchHandler index wiring with
    exthttp.RegisterRevisionedHandler, so the index ETag is centralized in the SDK
    and reflects registration state instead of process start. Bumps extension-kit
    to v1.11.0 and the SDK kits to the releases that bump the revision on
    register/clear.

--- main.go
+++ main.go
@@ -4,8 +4,6 @@
 package main
 
 import (
-	"time"
-
 	_ "github.com/KimMachineGun/automemlimit" // By default, it sets `GOMEMLIMIT` to 90% of cgroup's memory limit.
 	"github.com/rs/zerolog"
 	"github.com/steadybit/action-kit/go/action_kit_api/v2"
@@ -24,8 +22,6 @@ import (
 	"github.com/steadybit/extension-kit/extsignals"
 )
 
-var startedAt = time.Now().Format(time.RFC3339)
-
 func main() {
 	extlogging.InitZeroLog()
 	extbuild.PrintBuildInformation()
@@ -41,7 +37,7 @@ func main() {
 	action_kit_sdk.RegisterAction(extmonitor.NewMonitorDowntimeAction())
 	extevents.RegisterEventListenerHandlers()
 
-	exthttp.RegisterHttpHandler("/", exthttp.IfNoneMatchHandler(func() string { return startedAt }, exthttp.GetterAsHandler(getExtensionList)))
+	exthttp.RegisterRevisionedHandler("/", getExtensionList)
 
 	action_kit_sdk.RegisterCoverageEndpoints()
 	extsignals.ActivateSignalHandlers()
